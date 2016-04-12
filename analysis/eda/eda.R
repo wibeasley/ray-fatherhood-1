fb042a02be4f68243b4fa3f979431a552d9d204d
Will Beasley <wibeasley@hotmail.com>
2016-04-12 01:14:07 -0500

    First EDA

---
+++ analysis/eda/eda.R
@@ -0,0 +1,66 @@
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+
+# ---- load-sources ------------------------------------------------------------
+#Load any source files that contain/define functions, but that don't load any other types of variables
+#   into memory.  Avoid side effects and don't pollute the global environment.
+# source("./SomethingSomething.R")
+
+
+# ---- load-packages -----------------------------------------------------------
+# library(ggplot2) #For graphing
+library(magrittr)
+requireNamespace("readr")
+requireNamespace("knitr")
+# requireNamespace("scales") #For formating values in graphs
+# requireNamespace("RColorBrewer")
+requireNamespace("dplyr")
+requireNamespace("TabularManifest") # devtools::install_github("Melinae/TabularManifest")
+
+
+# ---- declare-globals ---------------------------------------------------------
+options(show.signif.stars=F) #Turn off the annotations on p-values
+
+path_input <- "./data-unshared/derived/involvement-subject.csv"
+
+
+# ---- load-data ---------------------------------------------------------------
+ds <- readr::read_csv(path_input)
+
+rm(path_input)
+
+
+# ---- tweak-data --------------------------------------------------------------
+ds <- ds %>%
+  dplyr::filter(section_complete_count == 6L) %>%
+  as.data.frame()
+
+# ---- singular-columns --------------------------------------------------------
+
+# ---- na-columns --------------------------------------------------------------
+
+
+# ---- marginals --------------------------------------------------------------
+# sapply(names(ds), function(x) cat(paste0("TabularManifest::histogram_discrete(ds, \"", x, "\")\n"))) %>% invisible
+
+# TabularManifest::histogram_discrete(ds, "response_id")
+TabularManifest::histogram_discrete(ds, "one_child_at_least")
+TabularManifest::histogram_discrete(ds, "live_with_child")
+TabularManifest::histogram_discrete(ds, "sexual_orientation")
+TabularManifest::histogram_discrete(ds, "live_with_mother")
+TabularManifest::histogram_discrete(ds, "is_married")
+TabularManifest::histogram_discrete(ds, "married_duration")
+TabularManifest::histogram_discrete(ds, "education")
+TabularManifest::histogram_discrete(ds, "age")
+TabularManifest::histogram_discrete(ds, "income_category")
+TabularManifest::histogram_discrete(ds, "child_in_home_count")
+TabularManifest::histogram_discrete(ds, "race")
+TabularManifest::histogram_discrete(ds, "religion")
+TabularManifest::histogram_discrete(ds, "work_hours_outside")
+TabularManifest::histogram_discrete(ds, "work_hours_spouse_outside")
+TabularManifest::histogram_discrete(ds, "section_complete_count")
+TabularManifest::histogram_continuous(ds, "autonomy"       , bin_width=.1    , rounded_digits = 1)
+TabularManifest::histogram_continuous(ds, "competency"     , bin_width=.25   , rounded_digits = 1)
+TabularManifest::histogram_continuous(ds, "involvement"    , bin_width=.2    , rounded_digits = 1)
+TabularManifest::histogram_continuous(ds, "motivation"     , bin_width=.2    , rounded_digits = 1)
+TabularManifest::histogram_continuous(ds, "relatedness"    , bin_width=.2    , rounded_digits = 1)
+TabularManifest::histogram_continuous(ds, "satisfaction"   , bin_width=.5    , rounded_digits = 1)
---
+++ analysis/eda/eda.md
@@ -0,0 +1,39 @@
+# EDA of Scales
+
+<!--  Set the working directory to the repository's base directory; this assumes the report is nested inside of three directories.-->
+
+
+<!-- Set the report-wide options, and point to the external code file. -->
+
+
+<!-- Load the sources.  Suppress the output when loading sources. -->
+
+
+<!-- Load 'sourced' R files.  Suppress the output when loading packages. -->
+
+
+<!-- Load any global functions and variables declared in the R file.  Suppress the output. -->
+
+
+<!-- Declare any global functions specific to a Rmd output.  Suppress the output. -->
+
+
+<!-- Load the datasets. -->
+
+
+<!-- Tweak the datasets. -->
+
+
+
+# Summary
+
+### Notes
+
+### Unanswered questions
+
+
+
+
+# Marginals
+![](figure-png/marginals-1.png)![](figure-png/marginals-2.png)![](figure-png/marginals-3.png)![](figure-png/marginals-4.png)![](figure-png/marginals-5.png)![](figure-png/marginals-6.png)![](figure-png/marginals-7.png)![](figure-png/marginals-8.png)![](figure-png/marginals-9.png)![](figure-png/marginals-10.png)![](figure-png/marginals-11.png)![](figure-png/marginals-12.png)![](figure-png/marginals-13.png)![](figure-png/marginals-14.png)![](figure-png/marginals-15.png)![](figure-png/marginals-16.png)![](figure-png/marginals-17.png)![](figure-png/marginals-18.png)![](figure-png/marginals-19.png)![](figure-png/marginals-20.png)![](figure-png/marginals-21.png)
+
